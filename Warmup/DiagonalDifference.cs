65e5d7dcd15ba4bebe00492f781ab83d225f743f
Tarun <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2025-12-10 23:03:48 +0530

    Implement diagonalDifference function in C#

---
+++ Warmup/DiagonalDifference.cs
@@ -0,0 +1,70 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {int j=1,k=j,l=arr[0].Count-2,m=1;
+for(int i=1;i<arr[0].Count;i++){
+    
+    arr[0][0]+=arr[j][k];
+    arr[0][arr[0].Count-1]+=arr[m][l];
+    ++j;++k;
+    ++m;--l;
+    Console.WriteLine(arr[0][0]+" "+arr[0][arr[0].Count-1]);
+    
+    
+}
+
+if((arr[0][0]-(arr[0][arr[0].Count-1]))>1){
+    return arr[0][0]-arr[0][arr[0].Count-1];
+}
+else{
+    return -(arr[0][0]-arr[0][arr[0].Count-1]);
+}
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
